0bce9ee04913eb401e07b563d61da85029a32649
Guru18650 <guruwszystkiego@gmail.com>
2022-11-07 20:24:34 +0100

    Delete weathertest.py

--- weathertest.py
+++
@@ -1,4 +0,0 @@
-import requests, json
-w_response = requests.get("http://api.airvisual.com/v2/city?city=katowice&state=silesia&country=poland&key=945ef6f0-7f53-4d26-a4e8-736a88e40f3c")
-w_json = w_response.json()
-print(w_json["data"])
